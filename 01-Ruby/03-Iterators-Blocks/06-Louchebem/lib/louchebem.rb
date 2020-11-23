67a8be144bc7b346e754c73409c5b3622c8dc73d
Alexandru Sima <simaalexandru98@gmail.com>
2020-11-23 20:06:03 +0100

    louchebem

--- 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
@@ -1,3 +1,16 @@
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+  suf = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+  array = sentence.split(/\b/)
+  array.map! do |word|
+    return word unless word.size > 1
+
+    if %w(a e i o u).include?(word[0])
+      'l' + word + suf.sample
+    else
+      firstt = word.split('').find_index { |vowel| %w(a e i o u).include?(vowel) }
+      'l' + word[firstt...word.size] + word[0...firstt] + suf.sample
+    end
+  end
+  array.join
 end
